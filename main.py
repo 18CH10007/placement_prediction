e92589615446c291d4d5301a42c6d86cec0505b3
ANIKET KUMAR <45457557+18CH10007@users.noreply.github.com>
2021-11-29 15:02:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -218,24 +218,3 @@ fig.update_layout(title_x=0.5,
 
 fig.show()
 
-## Hyper Parameter Optimization
-
-params={
- "learning_rate"    : [0.05, 0.10, 0.15, 0.20, 0.25, 0.30 ] ,
- "max_depth"        : [ 3, 4, 5, 6, 8, 10, 12, 15],
- "min_child_weight" : [ 1, 3, 5, 7 ],
- "gamma"            : [ 0.0, 0.1, 0.2 , 0.3, 0.4 ],
- "colsample_bytree" : [ 0.3, 0.4, 0.5 , 0.7 ]
-    
-}
-
-def timer(start_time=None):
-    if not start_time:
-        start_time = datetime.now()
-        return start_time
-    elif start_time:
-        thour, temp_sec = divmod((datetime.now() - start_time).total_seconds(), 3600)
-        tmin, tsec = divmod(temp_sec, 60)
-        print(f'\nTime taken: {thour} hours {tmin} minutes and {round(tsec, 2)} seconds.')
-        
- xgb_classifier = XGBClassifier()
